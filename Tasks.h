0ea81189660138433a79b52dfd47842f50510bbd
Robo_Start <138618375+RoboStartCoder@users.noreply.github.com>
2026-08-08 16:17:37 +0300

    Create Tasks.h

---
+++ Tasks.h
@@ -0,0 +1,24 @@
+#ifndef TASKS_H
+#define TASKS_H
+enum GlobalTasks {
+    INIT = 0,
+    RESET = 1
+};
+enum class StepperTasks {
+    STOP = 50,
+    FOR = 51,
+    UNTIL = 52
+};
+enum DebuggerTasks {
+    STATUS = 100
+};
+enum NetworkTasks {
+    CONNECT = 200,
+    CONNECTED = 201,
+    RECEIVE = 202,
+    SEND = 203
+};
+enum ModuleTasks {
+ 
+};
+#endif //TASKS_H
