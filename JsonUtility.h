b1bb6057ff1b05e6d2b885e0c3678f682b39ab81
Robo_Start <138618375+RoboStartCoder@users.noreply.github.com>
2026-08-08 16:35:43 +0300

    Create JsonUtility.h

---
+++ JsonUtility.h
@@ -0,0 +1,20 @@
+//
+// Created by Robo_Start on 03.08.2026.
+//
+ 
+#ifndef JSONUTILITY_H
+#define JSONUTILITY_H
+#include <Arduino.h>
+#include <ArduinoJson.h>
+ 
+class JsonUtility {
+public:
+    static bool jsonFromString(const String &input, JsonDocument &doc);
+ 
+    static String jsonToString(const JsonDocument &doc, bool prettify);
+ 
+    static void jsonToSerial(const JsonDocument &doc, HardwareSerial &serial);
+};
+ 
+ 
+#endif //JSONUTILITY_H
